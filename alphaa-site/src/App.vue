a084aa691e89e383f546baa3bcc6f3fb950d844a
Michael Helms <mhelms92@gmail.com>
2019-08-23 23:11:09 -0700

    initial push

---
+++ alphaa-site/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+       <span>Alpha Andromeda</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+            <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">BandCamp</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Spotify</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Facebook</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Youtube</span>
+      </v-btn>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Merch</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Instagram</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <Main/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Main from './components/Main';
+
+export default {
+  name: 'App',
+  components: {
+    Main
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ alphaa-site/src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-container>
+        <v-layout
+        text-center
+        wrap
+        >
+              <v-flex xs12>
+        <v-img
+          :src="require('../../public/aalogo.png')"
+          class="my-3"
+          contain
+          height="700"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          New Single Gaze Into The Void releasing September 5th
+        </h1>
+        <p class="subheading font-weight-regular">
+          Steam us on Spotify or find us on Bandcamp
+          <br>Shows announcing soon!
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        </p>
+      </v-flex>
+        </v-layout>
+    </v-container>
+</template>
