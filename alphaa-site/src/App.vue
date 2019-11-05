9947aaf96d86556d8b77106eb394a4a3f412a3bb
mhelms44 <mhelms92@gmail.com>
2019-11-05 13:49:26 -0500

    Displayport collapse handled slightly better, still optimizing for mobile. Added some data for menu export.

--- alphaa-site/src/App.vue
+++ alphaa-site/src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-       <span>Alpha Andromeda</span>
-      </v-toolbar-title>
+      <v-toolbar>
       <v-spacer></v-spacer>
+      <v-toolbar-items class=hidden-sm-and-down>
             <v-btn
         text
         href="https://alphaandromeda.bandcamp.com/releases"
         target="_blank"
+
       >
         <span class="mr-2">BandCamp</span>
       </v-btn>
@@ -54,6 +54,65 @@
       >
         <span class="mr-2">Merch</span>
       </v-btn>
+      </v-toolbar-items>
+
+      <!--Sets up smaller viewport menu to be replaced with hamburger eventually-->
+
+      <v-toolbar-items class=hidden-md-and-up>
+                    <v-btn
+        text
+        href="https://alphaandromeda.bandcamp.com/releases"
+        target="_blank"
+
+      >
+        <span class="mr-0">BandCamp</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://www.facebook.com/alphaandromedaband"
+        target="_blank"
+      >
+        <span class="mr-0">Facebook</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://www.instagram.com/alphaandromedaband/"
+        target="_blank"
+      >
+        <span class="mr-0">Instagram</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://discord.gg/wcUUZv5"
+        target="_blank"
+      >
+        <span class="mr-0">Discord</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://open.spotify.com/artist/44Imd1962FI94frqkxG1rK?si=mqFo84CnQZ-PLhTyTr5dFQ"
+        target="_blank"
+      >
+        <span class="mr-0">Spotify</span>
+      </v-btn>
+      <v-btn
+        text
+        href="https://www.youtube.com/channel/UC9IFkuLhrSLkGNDrE0h3GWw"
+        target="_blank"
+      >
+        <span class="mr-0">YouTube</span>
+      </v-btn>
+            <v-btn
+        text
+        href="https://alphaandromeda.bigcartel.com/"
+        target="_blank"
+      >
+        <span class="mr-0">Merch</span>
+            </v-btn>
+
+
+      </v-toolbar-items>
+      </v-toolbar>
     </v-app-bar>
 
     <v-content>
@@ -71,7 +130,13 @@ export default {
     Main
   },
   data: () => ({
-    //
-  }),
+    item: 1,
+    items: [
+      {
+        title:'BandCamp',
+        href:'https://alphaandromeda.bandcamp.com/releases'
+      }
+    ]
+  })
 };
 </script>
